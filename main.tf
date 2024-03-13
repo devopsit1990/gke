f55bbde0fa1acc909a64a332fba677296601da5a
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 14:29:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc_network" {
   source = "./modules/network/vpc/"
 
   name_prefix = "${var.cluster_name}-network-${random_string.suffix.result}"
-  project     = var.project
-  region      = var.region
+  project     = var.project_name
+  region      = var.region_name
 
   
