a60fc6c08c530af496836cc6a451f9e2d6670433
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 13:51:55 +0530

    Update main.tf

--- modules/network/vpc/main.tf
+++ modules/network/vpc/main.tf
@@ -4,3 +4,21 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
   mtu                     = 1460
 }
+
+resource "google_compute_subnetwork" "Node_network" {
+  name          = "var.Nodenetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.custom.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
+  }
+}
+
+
