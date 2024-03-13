b53b25eea382130056a1080caaccf3a626ad0236
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 15:37:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/home/mcarohit421/terraform/kubernetes-411307-78665b4908eb.json")}"
+  credentials = "${file("/home/mcarohit421/terraform/gke/kubernetes-411307-78665b4908eb.json")}"
   project     = "kubernetes-411307"
   region      = "us-central1"
 }
