82af96ad13c81bd30b75a2f8fedbbc15c4ba9613
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 13:29:04 +0530

    Create variables.tf

---
+++ modules/network/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "The project ID to host the cluster in (required)"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "The name of the cluster (required)"
+}
