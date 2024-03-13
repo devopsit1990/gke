b034a91fac87db71f11d895b0b7ca0e6a6c9c2b9
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 15:08:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("kubernetes-411307-78665b4908eb.json")}"
+  credentials = "${file("./.json")}"
   project     = "kubernetes-411307"
   region      = "us-central1"
 }
