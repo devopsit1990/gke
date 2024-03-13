49225e3ea1cfb715b53f103955828a88d948b2bf
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-13 15:52:46 +0530

    Update main.tf

--- modules/network/vpc/main.tf
+++ modules/network/vpc/main.tf
@@ -1,12 +1,12 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = [var.project_name]
-  name                    = [var.vpc_name]
+  project                 = var.project_name
+  name                    = var.vpc_name
   auto_create_subnetworks = false
   mtu                     = 1460
 }
 
 resource "google_compute_subnetwork" "Node_network" {
-  name          = [var.Nodenetwork]
+  name          = var.Nodenetwork
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
